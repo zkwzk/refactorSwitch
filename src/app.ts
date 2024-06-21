8fae18753d7aae576c38072a1c0cae61d3f82588
Zhengkun Wang <zkwang@thoughtworks.com>
2024-06-21 11:51:37 +0800

    [ZK] clean up project

--- src/app.ts
+++
@@ -1,2 +0,0 @@
-// This is the main logic of your Node.js application
-// Add your code here
--- tests/app.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('App', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
-  });
-});
